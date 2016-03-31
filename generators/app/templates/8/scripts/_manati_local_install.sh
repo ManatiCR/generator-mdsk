4efc0a8f15f6ba2ae247ce88aa77f05347dff348
Kevin Porras <kporras07@gmail.com>
2016-03-30 22:04:42 -0600

    5: Handle possibly absent config.

--- generators/app/templates/8/scripts/_manati_local_install.sh
+++ generators/app/templates/8/scripts/_manati_local_install.sh
@@ -3,7 +3,7 @@ DRUSH="./vendor/bin/drush"
 SITE_ALIAS="@<%= appName %>.<%= appName %>.dev"
 $DRUSH $SITE_ALIAS cc drush
 echo "Installing..."
-$DRUSH $SITE_ALIAS si <%= appName %> --account-pass=admin --config-dir=sites/default/files/config/sync -y
+if [ -f ./files/config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS si <%= appName %> --config-dir=sites/default/files/config/sync -y ; else $DRUSH $SITE_ALIAS si <%= appName %> -y ; fi
 #echo "Setting master scope..."
 #$DRUSH $SITE_ALIAS master-set-current-scope local
 #echo "Executing master..."
