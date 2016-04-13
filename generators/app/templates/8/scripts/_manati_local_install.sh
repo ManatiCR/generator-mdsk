e63ab127d41f4128c8a3637c66675daee009c4c2
Kevin Porras <kporras07@gmail.com>
2016-04-12 21:05:36 -0600

    5: Fix drush alias in install script.

--- generators/app/templates/8/scripts/_manati_local_install.sh
+++ generators/app/templates/8/scripts/_manati_local_install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DRUSH="./vendor/bin/drush"
-SITE_ALIAS="@<%= appName %>.<%= appName %>.dev"
+SITE_ALIAS="@<%= appName %>.<%= dashedAppName %>.dev"
 $DRUSH $SITE_ALIAS cc drush
 echo "Installing..."
 if [ -f ./files/config/sync/core.extension.yml ] ; then $DRUSH $SITE_ALIAS si standard --config-dir=sites/default/files/config/sync --account-pass=admin -y ; else $DRUSH $SITE_ALIAS si standard --account-pass=admin -y ; fi
