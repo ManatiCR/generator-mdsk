e05102b4371fd7eb13c506dd77f68d2d2d0157b3
Kevin Porras <kporras07@gmail.com>
2016-12-02 08:26:18 -0600

    First attempt to make behat work for D8.

---
+++ generators/app/templates/8/tests/behat/features/bootstrap/bootstrap/Devinci/DevinciExtension/Context/DebugContext.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Javascript context.
+ */
+
+namespace Devinci\DevinciExtension\Context;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * Defines application features from the specific context.
+ */
+class DebugContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+  /**
+   * Initializes context.
+   *
+   * Every scenario gets its own context instance.
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct($asset_dump_path) {
+  }
+
+}
