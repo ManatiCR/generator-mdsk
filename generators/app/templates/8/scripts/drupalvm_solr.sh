fb2078b115590c1627dc69b074b715898d8b4923
Kevin Porras <kporras07@gmail.com>
2017-01-20 08:59:05 -0600

    Update drupalvm_solr script for drupal 8 projects.

--- generators/app/templates/8/scripts/drupalvm_solr.sh
+++ generators/app/templates/8/scripts/drupalvm_solr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-vagrant ssh -c "wget -qO- http://ftp.drupal.org/files/projects/apachesolr-7.x-1.x-dev.tar.gz | tar -xz \
-  && sudo cp -r apachesolr/solr-conf/solr-4.x/* /var/solr/collection1/conf/ \
+vagrant ssh -c "wget -qO- http://ftp.drupal.org/files/projects/search_api_solr-8.x-1.x-dev.tar.gz | tar -xz \
+  && sudo cp -r search_api_solr/solr-conf/4.x/* /var/solr/collection1/conf/ \
   && sudo chown -R solr:solr /var/solr/collection1/conf/* \
-  && sudo service solr restart"
+  && sudo /etc/init.d/solr restart"
