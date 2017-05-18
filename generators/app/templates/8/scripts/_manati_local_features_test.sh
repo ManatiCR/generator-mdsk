162910ebc3fb7de6886e62bd87ad269c9b14624b
Kevin Porras <kporras07@gmail.com>
2017-05-18 10:29:36 -0600

    Fix vendor path.

--- generators/app/templates/8/scripts/_manati_local_features_test.sh
+++ generators/app/templates/8/scripts/_manati_local_features_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-DRUSH="./.vendor/bin/drush"
+DRUSH="./vendor/bin/drush"
 SITE_ALIAS="@<%= appName %>.<%= appName %>.dev"
 $DRUSH $SITE_ALIAS fl | grep -qi 'overridden' && (echo 'Overridden Features: fail' && exit 1) || (echo 'Overridden Features: pass' && exit 0)
